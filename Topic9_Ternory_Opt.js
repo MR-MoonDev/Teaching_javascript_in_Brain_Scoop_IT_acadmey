2676878b22648b4052de167c9b7fa8430e07a02e
Muneeb <muhammadmuneebulraza@gmail.com>
2025-01-19 00:55:26 -0800

    Rename Ternory_Opt to Topic9_Ternory_opt

--- Topic9_Ternory_Opt.js
+++ Topic9_Ternory_Opt.js
@@ -4,6 +4,8 @@ let grade = (score >= 90) ? "A" :
             (score >= 70) ? "C" : 
             (score >= 60) ? "D" : "F";
 console.log(grade); 
+
+
 let number = 5;
 let result = (number % 2 === 0) ? "Even" : "Odd";
 console.log(result); 
