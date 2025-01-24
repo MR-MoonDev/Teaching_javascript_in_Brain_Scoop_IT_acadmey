49e885679284cf58d61f60959b000dfcf470ddae
Moon <muhammadmuneebulraza@gmail.com>
2025-01-24 03:07:39 -0800

    Teach basic of function and their purpose

--- Topic13_Function.js
+++ Topic13_Function.js
@@ -1,15 +1,37 @@
-function functionName() {
-    document.getElementById("fun").innerHTML += ("Muneeb" +"</br>");
-    return;
+// Function without parameters
+function displayMessageWithoutParameter() {
+    document.getElementById("fun").innerHTML += 
+        "FUNCTION WITHOUT PARAMETER</br>";
 }
 
-functionName();
+displayMessageWithoutParameter();
 
-
-function add(a, b) {
-    return a + b;
+// Function with parameters
+function displayMessageWithParameter(message) {
+    document.getElementById("fun").innerHTML += `${message}</br>`;
 }
 
-document.getElementById("fun").innerHTML += (add(2, 3)+"</br>");
+displayMessageWithParameter("FUNCTION WITH PARAMETER");
+
+// Function without a return value
+function displaySumWithoutReturn(num1, num2) {
+    document.getElementById("fun").innerHTML += `${num1 + num2}</br>`;
+}
+
+displaySumWithoutReturn(10, 25);
+
+// Function with a return value
+function calculateSum(num1, num2) {
+    let sum = num1 + num2; // Calculate the sum
+    return sum; // Return the numeric result
+}
+
+let sumResult = calculateSum(10, 20); // Store the result of the sum
+document.getElementById("fun2").innerHTML += `${sumResult}</br>`;
+
+// Add 2 to the result of the sum and display
+let sumResultWithExtra = calculateSum(10, 20) + 2;
+document.getElementById("fun2").innerHTML += `${sumResultWithExtra}</br>`;
+
 
 
