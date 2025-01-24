eaa9dc76d70762bbbf2193a4eccb97754c0a40ba
Moon <muhammadmuneebulraza@gmail.com>
2025-01-24 01:01:33 -0800

    Teach Ternaory operator

--- Topic9_Ternory_Opt.js
+++ Topic9_Ternory_Opt.js
@@ -1,2 +1,7 @@
-let a = 2;
-(a === 2)?document.getElementById("Tarnaor_opt").innerHTML=(true):document.getElementById("Tarnaor_opt").innerHTML=(false)
+// let a = 2;
+// (a === 2)?document.getElementById("Tarnaor_opt").innerHTML=(true):document.getElementById("Tarnaor_opt").innerHTML=(false)
+
+let new_num = 9;
+(new_num === 7) ? console.log("eqaul to 7") :
+(new_num === 9) ? console.log("eqaul to 9") : 
+console.log("Not eqaul ")
