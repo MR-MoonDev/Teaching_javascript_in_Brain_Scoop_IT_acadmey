b9d122c5cbb7dbf73849dd0c97cf61561e521427
Moon <muhammadmuneebulraza@gmail.com>
2025-01-26 01:29:18 -0800

    practice of call back function and set timeout function

--- Topic13_Function.js
+++ Topic13_Function.js
@@ -1,17 +1,14 @@
 // Function without parameters
 
-
 function displayMessageWithoutParameter() {
-    document.getElementById("fun").innerHTML +=
-        "FUNCTION WITHOUT PARAMETER</br>";
+  document.getElementById("fun").innerHTML += "FUNCTION WITHOUT PARAMETER</br>";
 }
 displayMessageWithoutParameter();
 
 // Function with parameters
 
-
 function displayMessageWithParameter(message) {
-    document.getElementById("fun").innerHTML += `${message}</br>`;
+  document.getElementById("fun").innerHTML += `${message}</br>`;
 }
 
 displayMessageWithParameter("FUNCTION WITH PARAMETER");
@@ -19,58 +16,52 @@ displayMessageWithParameter("FUNCTION WITH PARAMETER");
 // Function without a return value
 
 function displaySumWithoutReturn(num1, num2) {
-    document.getElementById("fun").innerHTML += `${num1 + num2}</br>`;
+  document.getElementById("fun").innerHTML += `${num1 + num2}</br>`;
 }
 
 displaySumWithoutReturn(10, 25);
 
 // Function with a return value
 
-
 function calculateSum(num1, num2) {
-    let sum = num1 + num2;
-    return sum; // Return the numeric result
+  let sum = num1 + num2;
+  return sum; // Return the numeric result
 }
 
 let sumResult = calculateSum(10, 20);
 document.getElementById("fun2").innerHTML += `${sumResult}</br>`;
 
-
 // Add 2 to the result of the sum and display
-
 
 let sumResultWithExtra = calculateSum(10, 20) + 2;
 document.getElementById("fun2").innerHTML += `${sumResultWithExtra}</br>`;
 
-
 //  arrow functon
 
 const arrow_fun = () => {
-    document.getElementById("fun2").innerHTML += `${" arrow function"}</br>`;
-
-}
+  document.getElementById("fun2").innerHTML += `${" arrow function"}</br>`;
+};
 
 let arr_fun_holder = arrow_fun();
-document.getElementById("fun2").innerHTML += `Arrow function without parameter ${arr_fun_holder}</br>`;
+document.getElementById(
+  "fun2"
+).innerHTML += `Arrow function without parameter ${arr_fun_holder}</br>`;
 
 // arrow function with parameter
 
 const arrow_fun_with_parameter = (a, b) => {
-    let arrow_fun_holder = a + b;
-    return arrow_fun_holder;
-}
+  let arrow_fun_holder = a + b;
+  return arrow_fun_holder;
+};
 
-let a = arrow_fun_with_parameter(2, 3)
+let a = arrow_fun_with_parameter(2, 3);
 let arrow_fun_with_parameter_holder = a;
-document.getElementById("fun2").innerHTML += `Arrow function with parameter ${arrow_fun_with_parameter_holder}</br>`;
+document.getElementById(
+  "fun2"
+).innerHTML += `Arrow function with parameter ${arrow_fun_with_parameter_holder}</br>`;
 
-
-function get_name_fun(){
-
-var takename =    document.getElementById("getname").value;
-document.getElementById("fun2").innerHTML += ("Hello "+ takename + '</br>')
-document.getElementById("getname").value = "";// input clear
+function get_name_fun() {
+  var takename = document.getElementById("getname").value;
+  document.getElementById("fun2").innerHTML += "Hello " + takename + "</br>";
+  document.getElementById("getname").value = ""; // input clear
 }
-
-
-
---
+++ Topic14_Callback_fun.js
@@ -0,0 +1,49 @@
+// CallBack funtion
+
+function fun_One(call_back) {
+  document.getElementById("callBack_funID").innerHTML += "hello" + "</br>";
+  call_back();
+}
+
+function callback() {
+  document.getElementById("callBack_funID").innerHTML +=
+    "i'm call back function" + "</br>";
+}
+
+fun_One(callback);
+
+// setTimeout(function, delay, param1, param2, ...);
+
+// set time out
+
+setTimeout(() => {
+  document.getElementById("callBack_funID").innerHTML +=
+    "This runs after 2 seconds" + "</br>";
+}, 2000);
+
+function setTime_fun(name, age) {
+  document.getElementById("callBack_funID").innerHTML +=
+    "Set Time out Function " + name + age + "</br>";
+}
+setTimeout(setTime_fun, 3000, "Muneeb", 12);
+setTimeout(() => setTime_fun("Muneeb", 22), 3000);
+
+//   Asynchronous function
+
+function fetching_data(callBack) {
+  document.getElementById("callBack_funID").innerHTML +=
+    "Fetching data..." + "</br>";
+
+  setTimeout(() => {
+    document.getElementById("callBack_funID").innerHTML +=
+      "Data fetched." + "</br>";
+    callBack();
+  }, 6000);
+}
+
+function processingdata() {
+  document.getElementById("callBack_funID").innerHTML +=
+    "Processing data " + "</br>";
+}
+
+fetching_data(processingdata);
