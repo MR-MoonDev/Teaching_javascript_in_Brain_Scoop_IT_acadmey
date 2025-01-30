285586b7a7c4cdff692765f3b32ef41f3e6af2ea
Moon <muhammadmuneebulraza@gmail.com>
2025-01-30 15:27:05 -0800

    teach id selector and Class selector and forther are cotinue

--- Topic18_Selection_Methods.js
+++ Topic18_Selection_Methods.js
@@ -1,2 +1,15 @@
-document.getElementById("id_selection").innerHTML=("By ID selection")
+//id selection
+document.getElementById("id_selection").innerHTML=("This text in JS");
+
+// only index elements with the class name "class_selection" will be changed
+
+document.getElementsByClassName("class_selection")[0].innerHTML=("This text  class selection in JS");
+document.getElementsByClassName("class_selection")[1].innerHTML=("This text class selection in JS");
+
+// all elements with the class name "class_selection" will be changed
+
+let elements = document.getElementsByClassName("class_selection");
+for(i = 0; i < elements.length; i++){
+   elements[i].innerHTML=("This text class selection in JS");
+}
 
