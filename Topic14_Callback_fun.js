a0681097135b481c33d5bcc17ea036309cedacb7
Moon <muhammadmuneebulraza@gmail.com>
2025-03-09 18:18:11 -0700

    teach call back function

--- Topic14_Callback_fun.js
+++ Topic14_Callback_fun.js
@@ -1,31 +1,38 @@
 // CallBack funtion
 
 function fun_One(call_back) {
-  document.getElementById("callBack_funID").innerHTML += "hello" + "</br>";
+  document.getElementById("callBack_funID").innerHTML += "hello ";
   call_back();
 }
 
 function callback() {
-  document.getElementById("callBack_funID").innerHTML +=
-    "i'm call back function" + "</br>";
+  document.getElementById("callBack_funID").innerHTML += "i'm call back function" + "</br>";
 }
-
+ 
 fun_One(callback);
 
 // setTimeout(function, delay, param1, param2, ...);
 
 // set time out
 
+t = 2000
+
 setTimeout(() => {
-  document.getElementById("callBack_funID").innerHTML +=
-    "This runs after 2 seconds" + "</br>";
-}, 2000);
+  document.getElementById("callBack_funID").innerHTML +="This runs after 2 seconds" + "</br>";
+},t);
+
+
+setTimeout(() => {
+  document.getElementById("callBack_funID").innerHTML +="This runs after 3 seconds" + "</br>";
+  
+}, 3000);
 
 function setTime_fun(name, age) {
-  document.getElementById("callBack_funID").innerHTML +=
-    "Set Time out Function " + name + age + "</br>";
+  document.getElementById("callBack_funID").innerHTML += "Set Time out Function " + name + age + "</br>";
 }
 setTimeout(setTime_fun, 3000, "Muneeb", 12);
+
+
 setTimeout(() => setTime_fun("Muneeb", 22), 3000);
 
 //   Asynchronous function
--- Topic15_Array.js
+++ Topic15_Array.js
@@ -2,7 +2,7 @@
 
 let arrayname = ["item1", "item2", "item3"];
 
-document.getElementById("array").innerHTML += `${arrayname}<br>`;
+document.getElementById("array").innerHTML += `${arrayname[2]}<br>`;
 
 // after creating an array, we can add more elements to it
 
@@ -13,12 +13,14 @@ document.getElementById("array").innerHTML += `${arrayname}<br>`;
 
 let fruit_array = [];
 
-fruit_array[0] = "item1";
-fruit_array[1] = "item2";
-fruit_array[3] = "item3";
+fruit_array[0] = "item-1";
+fruit_array[1] = "item-2";
+// fruit_array[2] = "item-4";
+fruit_array[3] = "item-3";
 document.getElementById("array").innerHTML += `${fruit_array}<br>`;
 
 // using new keyword
 
 let new_array = new Array("item1", "item2", "item3");
+
 document.getElementById("array").innerHTML += `${new_array}<br>`;
